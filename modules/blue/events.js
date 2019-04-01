b1cdfd7547c7b424ac82839071a7cbcc460ba54c
Jorge Vargas <jorgev259@outlook.es>
2019-04-01 09:57:30 -0600

    typo

--- modules/blue/events.js
+++ modules/blue/events.js
@@ -3,13 +3,15 @@ module.exports = {
   config: {
     default: true
   },
-  async ready (client, db) {
-    let guild = client.guilds.first()
-    guild.setName('Blueside Treachery Lore')
-    guild.setIcon(path.join(__dirname, 'resources/pfp.jpg'))
-    guild.channels.find(c => c.name === 'general').send(
-      '@everyone',
-      { files: [path.join(__dirname, 'resources/lived.png')] }
-    )
+  events: {
+    async ready (client, db) {
+      let guild = client.guilds.first()
+      guild.setName('Blueside Treachery Lore')
+      guild.setIcon(path.join(__dirname, 'resources/pfp.jpg'))
+      guild.channels.find(c => c.name === 'general').send(
+        '@everyone',
+        { files: [path.join(__dirname, 'resources/lived.png')] }
+      )
+    }
   }
 }
