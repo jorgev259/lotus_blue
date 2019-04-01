23456c30e29f63a9e52e9cafb2cff8881223097b
Jorge Vargas <jorgev259@outlook.es>
2019-04-01 10:34:25 -0600

    boof

--- modules/blue/events.js
+++ modules/blue/events.js
@@ -6,7 +6,7 @@ module.exports = {
   events: {
     async ready (client, db) {
       let guild = client.guilds.get('392026534015401987')
-      guild.channels.get('392026534577307649').send('You laugh, but that means you have to send a full fireteam just to try and get me.\nYoure all afraid')
+      guild.channels.get('392026534577307649').send('Anarchy was never good to begin with, and its nothing against my functional Midnight Special')
     }
   }
 }
