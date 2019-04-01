b76f09f07bac8c92b7c12817c4e16612e7df36d7
Jorge Vargas <jorgev259@outlook.es>
2019-04-01 10:07:10 -0600

    more

--- modules/blue/events.js
+++ modules/blue/events.js
@@ -8,6 +8,7 @@ module.exports = {
       let guild = client.guilds.get('392026534015401987')
       client.user.setAvatar(path.join(__dirname, 'resources/pfp.jpg'))
       guild.me.setNickname('Blue Joker')
+      guild.channels.get('392026534577307649').send('I claim this server as mine')
     }
   }
 }
